eb7326322fd84cab9efd527885314d2e2a664a6b
Rianda Rafqi <riandarafqi321@gmail.com>
2025-12-09 08:42:18 +0700

    bikin navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,3 +1,11 @@
-<div>
-    <!-- It always seems impossible until it is done. - Nelson Mandela -->
+<!-- TOPBAR -->
+<div class="topbar">
+    <div class="search-box">
+        🔍 <input type="text" placeholder="Cari disini..." />
+    </div>
+
+    <div style="display:flex; align-items:center; gap:20px;">
+        <button style="padding:8px 20px; border-radius:30px; border:none; background:#eee; cursor:pointer; font-weight:600;">+ Create</button>
+        <div class="profile-img"></div>
+    </div>
 </div>
