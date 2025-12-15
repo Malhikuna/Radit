d58beab7d2697369857b30712df93905397111e3
dwiyyl <dwiyulianti327@gmail.com>
2025-12-15 22:50:27 +0700

    feat: membuat Sorting (New dan Best)

---
+++ app/Livewire/PostList.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+use App\Models\Post;
+
+class PostList extends Component
+{
+    public $sort = 'new';
+
+    public function render()
+    {
+        $query = Post::withSum('votes', 'value');
+
+        if ($this->sort === 'best') {
+            $query->orderByDesc('votes_sum_value');
+        } else {
+            $query->orderByDesc('created_at');
+        }
+
+        $posts = $query->get();
+
+        return view('livewire.post-list', compact('posts'));
+    }
+}
