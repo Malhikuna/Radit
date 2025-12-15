70f2fb1589b27bd1e569c0cf664babd0fd5e3eee
Rianda Rafqi <riandarafqi321@gmail.com>
2025-12-15 08:38:29 +0700

    download livewire

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,14 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Livewire\Counter;
+use App\Livewire\Home;
 
 Route::get('/counter', Counter::class);
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 Route::get('/create-thread', function () {
     return view('thread.create');
 });
+Route::get('/', Home::class);
