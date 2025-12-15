5b381eeaafdf9750754f2750ac5f6a2119783f35
Danny <danny221004@gmail.com>
2025-12-15 18:13:13 +0700

    feat: Menambahkan autentikasi  login dan registrasi pengguna

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-use App\Livewire\Counter;
-
-Route::get('/counter', Counter::class);
+use App\Livewire\Auth\Login;
+use App\Livewire\Auth\Register;
+use App\Http\Controllers\SocialAuthController;
 
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/create-thread', function () {
-    return view('thread.create');
-});
+
+Route::get('/login', Login::class)->name('login');
+Route::get('/register', Register::class)->name('register');
+
+Route::get('/auth/{provider}', [SocialAuthController::class, 'redirect']);
+Route::get('/auth/{provider}/callback', [SocialAuthController::class, 'callback']);
