ebacafb6d70a89d6e8a2016a47b6cd4e6d61b795
Rianda Rafqi <riandarafqi321@gmail.com>
2025-12-15 08:38:29 +0700

    download livewire

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>@yield('title', 'Enable404')</title>
     <script src="https://cdn.tailwindcss.com"></script>
+    @livewireStyles
 </head>
 
 <body class="bg-gray-50">
@@ -19,10 +20,12 @@
 
         {{-- MAIN CONTENT --}}
         <main class="flex-1 p-8">
-            @yield('content')
+            {{ $slot }}
         </main>
 
     </div>
+
+    @livewireScripts
 
 </body>
 </html>
