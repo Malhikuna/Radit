c82ba0d211994552a1049b07a862e319bc7eea01
dwiyyl <dwiyulianti327@gmail.com>
2025-12-19 23:17:41 +0700

    feat: add create community

--- app/Livewire/Pages/Community/Create.php
+++ app/Livewire/Pages/Community/Create.php
@@ -3,11 +3,38 @@
 namespace App\Livewire\Pages\Community;
 
 use Livewire\Component;
+use App\Models\Community;
 
 class Create extends Component
 {
+    public $name;
+    public $description;
+
+    protected $rules = [
+        'name' => 'required|min:3|unique:communities,name',
+        'description' => 'nullable|min:5',
+    ];
+
+    public function save()
+    {
+        $this->validate();
+
+        Community::create([
+            'name' => $this->name,
+            'description' => $this->description,
+        ]);
+
+        session()->flash('success', 'Community berhasil dibuat');
+
+        return redirect()->route('posts.create');
+        // return redirect()->route('home');
+    }
+
     public function render()
     {
-        return view('livewire.pages.community.create');
+        return view('livewire.pages.community.create')
+            ->layout('components.layout', [
+                'title' => 'Create Community'
+            ]);
     }
 }
