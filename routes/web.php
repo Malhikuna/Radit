3f9ddf126547f40f64fff12e59d182633fe16d52
dwiyyl <dwiyulianti327@gmail.com>
2025-12-20 11:18:58 +0700

    feat: add community index, edit, and delete

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,8 @@ use App\Http\Controllers\SocialAuthController;
 use App\Livewire\Pages\Home;
 use App\Livewire\Pages\Post\Create as PostCreate;
 use App\Livewire\Pages\Community\Create as CommunityCreate;
+use App\Livewire\Pages\Community\Index as CommunityIndex;
+use App\Livewire\Pages\Community\Edit as CommunityEdit;
 
 Route::get('/counter', Counter::class);
 
@@ -18,8 +20,27 @@ Route::get('/', Home::class)->name('home');
 Route::get('/create-thread', PostCreate::class)
     ->name('posts.create');
 
-Route::get('/communities/create', CommunityCreate::class)
-    ->name('communities.create');
+Route::prefix('communities')
+    ->name('communities.')
+    ->group(function () {
+
+        Route::get('/', CommunityIndex::class)
+            ->name('index');
+
+        Route::get('/create', CommunityCreate::class)
+            ->name('create');
+
+        Route::get('/{community}/edit', CommunityEdit::class)
+            ->name('edit');
+    });
+// Route::get('/communities/create', CommunityCreate::class)
+//     ->name('communities.create');
+
+// Route::get('/communities', CommunityIndex::class)
+//     ->name('communities.index');
+
+// Route::get('/communities/{community}/edit', CommunityEdit::class)
+//     ->name('communities.edit');
 
 Route::get('/login', Login::class)->name('login');
 Route::get('/register', Register::class)->name('register');
