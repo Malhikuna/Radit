c5458365e295c39c0d24ffb47863a558a990e05c
dwiyyl <dwiyulianti327@gmail.com>
2025-12-20 11:18:58 +0700

    feat: add community index, edit, and delete

--- app/Livewire/Pages/Community/Edit.php
+++ app/Livewire/Pages/Community/Edit.php
@@ -3,11 +3,39 @@
 namespace App\Livewire\Pages\Community;
 
 use Livewire\Component;
+use App\Models\Community;
 
 class Edit extends Component
 {
+    public Community $community;
+    public $name;
+    public $description;
+
+    public function mount(Community $community)
+    {
+        $this->community = $community;
+        $this->name = $community->name;
+        $this->description = $community->description;
+    }
+
+    public function update()
+    {
+        $this->validate([
+            'name' => 'required|min:3|unique:communities,name,' . $this->community->id,
+            'description' => 'nullable|min:5',
+        ]);
+
+        $this->community->update([
+            'name' => $this->name,
+            'description' => $this->description,
+        ]);
+
+        return redirect()->route('communities.index');
+    }
+
     public function render()
     {
-        return view('livewire.pages.community.edit');
+        return view('livewire.pages.community.edit')
+            ->layout('components.layout', ['title' => 'Edit Community']);
     }
 }
