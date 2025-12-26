da073839c0abfed189d86048612c10a173f70d88
hikmalmn <hikmal.233040091@mail.unpas.ac.id>
2025-12-26 23:34:05 +0700

    merge: resolve conflict on community/show.blade.php

--- resources/views/livewire/community/show.blade.php
+++ resources/views/livewire/community/show.blade.php
@@ -10,17 +10,7 @@
 
             {{-- ICON --}}
             <div
-<<<<<<< Updated upstream
-<<<<<<< HEAD
-                class="w-16 h-16 rounded-full bg-purple-500 text-white flex items-center justify-center text-2xl font-bold">
-=======
-                class="w-16 h-16 rounded-full bg-[#9966CC] border-[#7A49A6] text-white
-                       flex items-center justify-center text-2xl font-bold">
->>>>>>> b6fc0963f8d5fcdaac5cf6e334062f6bab56b8dc
-=======
-                class="w-16 h-16 rounded-full bg-[#3e2b2c] text-white
-                       flex items-center justify-center text-2xl font-bold">
->>>>>>> Stashed changes
+                class="w-16 h-16 rounded-full bg-[#9966CC] border-[#7A49A6] text-white flex items-center justify-center text-2xl font-bold">
                 {{ strtoupper(substr($community->name, 0, 1)) }}
             </div>
 
@@ -36,36 +26,14 @@
 
             {{-- ACTIONS --}}
             <div class="ml-auto flex items-center gap-3">
-<<<<<<< Updated upstream
-<<<<<<< HEAD
                 <a 
                     href="{{ route('posts.create') }}"
-                    class="px-4 py-2 rounded-full bg-purple-500 text-white font-semibold hover:bg-purple-600">
-=======
-                <a href="{{ route('posts.create') }}"
-                   class="px-4 py-2 rounded-full bg-[#9966CC]
-                          text-white font-semibold hover:bg-[#7A49A6]">
->>>>>>> b6fc0963f8d5fcdaac5cf6e334062f6bab56b8dc
-=======
-                <a href="{{ route('posts.create') }}"
-                   class="px-4 py-2 rounded-full bg-[#3e2b2c]
-                          text-white font-semibold hover:bg-[#3e2b2c]">
->>>>>>> Stashed changes
+                    class="px-4 py-2 rounded-full bg-[#9966CC] text-white font-semibold hover:bg-[#7A49A6]">
                     + Create Post
                 </a>
 
                 <button
-<<<<<<< Updated upstream
-<<<<<<< HEAD
-                    class="px-4 py-2 rounded-full border border-purple-500 text-purple-500 font-semibold hover:bg-purple-50">
-=======
-                    class="px-4 py-2 rounded-full border border-[#4480e7]
-                           text-white font-semibold hover:bg-[#4480e7] bg-[#6395ee]">
->>>>>>> b6fc0963f8d5fcdaac5cf6e334062f6bab56b8dc
-=======
-                    class="px-4 py-2 rounded-full border border-[#3e2b2c]
-                           text-[#3e2b2c] font-semibold hover:bg-[#3e2b2c]">
->>>>>>> Stashed changes
+                    class="px-4 py-2 rounded-full border border-[#4480e7] text-white font-semibold hover:bg-[#4480e7] bg-[#6395ee]">
                     Join
                 </button>
             </div>
