937d9ab26300a234c556031beae42927997456e8
hikmalmn <hikmal.233040091@mail.unpas.ac.id>
2025-12-26 09:44:21 +0700

    feat: added new folder

---
+++ app/Livewire/Search/PostList.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Search;
+
+use Livewire\Component;
+
+class PostList extends Component
+{
+    public function render()
+    {
+        return view('livewire.search.post-list');
+    }
+}
