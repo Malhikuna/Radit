529f1271968fefedfe35c8625b67cf984570e72f
hikmalmn <hikmal.233040091@mail.unpas.ac.id>
2025-12-29 20:59:20 +0700

    style(login,register): changed background color

--- resources/views/livewire/auth/register.blade.php
+++ resources/views/livewire/auth/register.blade.php
@@ -1,4 +1,4 @@
-<div class="min-h-screen flex items-center justify-center bg-gray-100">
+<div class="min-h-screen flex items-center justify-center bg-[#9966CC]/15">
 
     <div class="bg-white w-[360px] p-8 rounded-xl border border-gray-300 shadow-sm">
 
@@ -19,7 +19,7 @@
                 "
             >
             <span class="font-bold text-xl text-[#9966CC]">
-                RADIT
+                radit
             </span>
         </a>
 
